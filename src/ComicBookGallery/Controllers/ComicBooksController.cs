3277bb3a33b9fe9fff83fb50e50e1c88e7e56730
Tracey <twright48@email.davenport.com>
2017-02-06 13:26:26 -0500

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,7 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spiderman survives...<strong>will Peter Parker?</strong><p>";
 
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Elipolous"
+            };
 
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
             //{
